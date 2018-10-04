2cc4b03fe0825b48f22f1ff72e75cc88d124738f
Mona Lisa <official.suprabh@gmail.com>
2018-10-04 20:27:54 +0530

    Final commit

--- src/containers/chatWindow.js
+++ src/containers/chatWindow.js
@@ -1,8 +1,24 @@
 import React from "react";
+import store from "../store";
+import Chats from "../containers/chat"; 
+import Header from "../components/header";
+import './chatWindow.css';
+import _ from "lodash";
+import MessageInput from "./messageInput";
+
 
 const ChatWindow = ({ activeUserId }) => {
+  const state = store.getState();
+  const { typing } = state;
+  const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
+
   return (
-    <div className="ChatWindow">Conversation for user id: {activeUserId}</div>
+    <div className="ChatWindow">
+      <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
+    </div>
   );
 };
 
---
+++ src/containers/messageInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+import store from "../store";
+import { setTypingValue, sendMessage } from "../actions";
+import "./messageInput.css";
+
+const MessageInput = ({ value }) => {
+
+  const handleChange = e => {
+    store.dispatch(setTypingValue(e.target.value));
+  };
+
+  const state = store.getState();  
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { typing, activeUserId } = state;
+    store.dispatch(sendMessage(typing, activeUserId));
+  };
+
+  return (
+    <form className="Message" onSubmit={handleSubmit}>
+      <input
+        className="Message__input"
+        onChange={handleChange}
+        value={value}
+        placeholder="write a message"
+      />
+    </form>
+  );
+};
+
+export default MessageInput;
